c6f5ca52a3c4f1da9ed2a3dcd5a5ca5b909b7c73
Steven <servingbaby@users.noreply.github.com>
2023-04-20 10:29:10 -0500

    Adjusted to add yahoo session and crumb
    April 18-19th, Yahoo adjusted API to require additional parameters.

--- ticker.sh
+++ ticker.sh
@@ -20,6 +20,19 @@ FIELDS=(symbol marketState regularMarketPrice regularMarketChange regularMarketC
   preMarketPrice preMarketChange preMarketChangePercent postMarketPrice postMarketChange postMarketChangePercent)
 API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
 
+# Define yahooSession function to retrieve the session and crumb values
+yahooSession () {
+  h=$(curl -s -c cookies.txt 'https://finance.yahoo.com' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:118.0) Gecko/20100101 Firefox/118.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' --compressed)
+  n=$(date +%s%N | cut -b1-13)
+  query_srv="https://query$n.finance.yahoo.com/v1/test/getcrumb"
+  crumb=$(curl -s -b cookies.txt "$query_srv" -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:118.0) Gecko/20100101 Firefox/118.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' --compressed | grep -oP 'CrumbStore\":{\"crumb\":\"[^\"]+\"' | grep -oP ':[^:]+$' | tr -d '"')
+  echo "h=$h"
+  echo "crumb=$crumb"
+}
+
+# Call yahooSession to retrieve session and crumb values
+yahoo_session=$(yahooSession)
+
 if [ -z "$NO_COLOR" ]; then
   : "${COLOR_BOLD:=\e[1;37m}"
   : "${COLOR_GREEN:=\e[32m}"
@@ -30,11 +43,16 @@ fi
 symbols=$(IFS=,; echo "${SYMBOLS[*]}")
 fields=$(IFS=,; echo "${FIELDS[*]}")
 
-results=$(curl --silent "$API_ENDPOINT&fields=$fields&symbols=$symbols" \
-  | jq '.quoteResponse .result')
+results=$(curl --silent "$API_ENDPOINT&fields=$fields&symbols=$symbols&crumb=$crumb" \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3' \
+  -H 'DNT: 1' \
+  -H "Cookie: B=${session_handle}" \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache')
 
 query () {
-  echo $results | jq -r ".[] | select(.symbol == \"$1\") | .$2"
+  echo $results | jq -r ".quoteResponse.result[] | select(.symbol == \"$1\") | .$2"
 }
 
 for symbol in $(IFS=' '; echo "${SYMBOLS[*]}" | tr '[:lower:]' '[:upper:]'); do
