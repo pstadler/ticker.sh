cc87740f09d360bcb00706ecfc06ecf1c26ff466
Steven Bennett <4163156+appatalks@users.noreply.github.com>
2023-08-23 15:58:41 -0500

    NOCOLOR Compatibility

--- ticker.sh
+++ ticker.sh
@@ -10,19 +10,14 @@ COOKIE_FILE="${SESSION_DIR}/cookies.txt"
 API_ENDPOINT="https://query1.finance.yahoo.com/v8/finance/chart/"
 API_SUFFIX="?interval=1d"
 
+# Check if NO_COLOR is set to disable colorization
+if [ -z "$NO_COLOR" ]; then
+  : "${COLOR_GREEN:=$'\e[32m'}"
+  : "${COLOR_RED:=$'\e[31m'}"
+  : "${COLOR_RESET:=$'\e[00m'}"
+fi
+
 SYMBOLS=("$@")
-
-if ! $(type jq > /dev/null 2>&1); then
-  echo "'jq' is not in the PATH. (See: https://stedolan.github.io/jq/)"
-  exit 1
-fi
-
-if [ -z "$SYMBOLS" ]; then
-  echo "Usage: ./ticker.sh AAPL MSFT GOOG BTC-USD"
-  exit
-fi
-
-[ ! -d "$SESSION_DIR" ] && mkdir -m 700 "$SESSION_DIR"
 
 preflight () {
   curl --silent --output /dev/null --cookie-jar "$COOKIE_FILE" "https://finance.yahoo.com" \
@@ -51,15 +46,21 @@ for symbol in "${SYMBOLS[@]}"; do
   percentChange=$(python -c "print('{:.2f}'.format(($currentPrice - $previousClose) / $previousClose * 100))")
 
   if (( $(echo "$priceChange >= 0" | bc -l) )); then
-    color=$'\e[32m'
+    color="$COLOR_GREEN"
   elif (( $(echo "$priceChange < 0" | bc -l) )); then
-    color=$'\e[31m'
+    color="$COLOR_RED"
   fi
 
-  printf "%s%-10s%8.2f%10.2f%8s%6.2f%%%s\n" \
-    "$color" "$symbol" \
-    "$currentPrice" "$priceChange" "$color" "$percentChange" \
-    $'\e[0m'
+  if [ -z "$NO_COLOR" ]; then
+    printf "%s%-10s%8.2f%10.2f%8s%6.2f%%%s\n" \
+      "$color" "$symbol" \
+      "$currentPrice" "$priceChange" "$color" "$percentChange" \
+      "$COLOR_RESET"
+  else
+    # printf "%-10s%8.2f%10.2f%8s%6.2f%%\n" \
+    printf "%-10s%8.2f%10.2f%9.2f%%\n" \
+      "$symbol" \
+      "$currentPrice" "$priceChange" "$percentChange"
+  fi
 
 done
-
